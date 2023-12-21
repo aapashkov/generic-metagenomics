c2004267abf5c73068c4182b82f3b35ed89b56bf
aapashkov <pashkov@comunidad.unam.mx>
2023-12-21 01:17:24 -0600

    Add multiprocessing to map.py

--- src/map.py
+++ src/map.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
+from argparse import ArgumentParser
+from multiprocessing import Pool, cpu_count
 from pathlib import Path
 from subprocess import run
-import sys
+
+parser = ArgumentParser()
+parser.add_argument("CMD")
+parser.add_argument("-p", type=int, default=1)
+cmd, proc = vars(parser.parse_args()).values()
+proc = max(min(cpu_count(), proc), 1)
 
 base = Path(__file__).resolve().parent.parent
-cmd = sys.argv[1]
 with open(base/"accessions.txt") as file: accs = [l.strip() for l in file]
-for acc in accs: run([cmd, acc])
+def run_cmd(acc): run([cmd, acc])
+with Pool(proc) as pool: pool.map(run_cmd, accs)
