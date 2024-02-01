28b163ab268b07ec799b0785c40145b2d009761e
aapashkov <pashkov@comunidad.unam.mx>
2024-02-01 10:16:26 -0600

    Add BGC annotator

---
+++ src/annotate-bgcs.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# ----------------------------------------------------------------
+# Annotate reads with mifaser.
+# Usage: ./src/docker-run.sh src/annotate-functions.sh [accession]
+# ----------------------------------------------------------------
+
+log () {
+  echo "$(date +'%D %T:') ${1}" >&2
+}
+
+# Change to project base directory
+cd $(dirname $(dirname $(readlink -f $0)))
+
+# Get number of CPUs from config file
+cpus=$(cat cpus.conf)
+
+# Set input and output
+database="data/databases/antismashDB"
+inp="data/bins"
+out="data/bgcs"
+tmp=${out}"/.tmp-${1}"
+mkdir -m 775 -p "${tmp}"
+trap "rm -rf ${tmp}" EXIT
+
+# Skip accession if already annotated
+if [[ -f "${out}/${1}.tar.gz" ]]; then
+  log "  Skipping ${1}"
+else
+  log "  Annotating BGCs of ${1}"
+
+  # Decompress .fasta files from tar.gz and annotate them
+  tar -C "${tmp}" -vzxf "${inp}/${1}.tar.gz" --wildcards "*.fasta" \
+    | while read file; do
+
+    base=$(basename "${file}" .fasta)
+
+    antismash -c "${cpus}" \
+      --output-dir "${tmp}/${1}/${base}" \
+      --output-basename "${base}" \
+      --genefinding-tool prodigal-m \
+      --databases "${database}" \
+      "${tmp}/${file}"
+
+  done
+
+  # Compress into archive and move it out of temporary directory
+  rm "${tmp}/${1}/"*.fasta
+  tar -C "${tmp}/" -I "pigz -kp $cpus " -cf "${tmp}/${1}.tar.gz" "${1}"
+  mv "${tmp}/${1}.tar.gz" "${out}/."
+
+fi
