df4849d10b53fb65d6c0f99f971ca05b0e8abf09
aapashkov <pashkov@comunidad.unam.mx>
2024-02-05 13:12:52 -0600

    Multiple fixes to annotate-functions

--- src/annotate-functions.sh
+++ src/annotate-functions.sh
@@ -18,30 +18,36 @@ cd $(dirname $(dirname $(readlink -f $0)))
 inp="data/reads/trimmed"
 out="data/functions"
 tmp=${out}"/.tmp-${1}"
-mkdir -m 775 -p ${out}
+mkdir -m 775 -p ${tmp}
 trap "rm -rf ${tmp}" EXIT
 
 # Skip accession if already annotated
-if [[ -d "${out}/${1}.tar.gz" ]]; then
+if [[ -f "${out}/${1}.tar.gz" ]]; then
   log "  Skipping ${1}"
 else
-  log "  Annotating functions of ${1}"
 
   # Annotate files depending on file type (single or paired)
   if [[ -f "${inp}/${1}_1.fq.gz" ]]; then
 
     # Paired end read annotation
+    seqtk seq -A "${inp}/${1}_1.fq.gz" > "${tmp}/${1}_1.fa"
+    seqtk seq -A "${inp}/${1}_2.fq.gz" > "${tmp}/${1}_2.fa"
     mifaser -s 0 -S 0 -q -o "${tmp}" -d "GS-21-all" \
-      -l "${inp}/${1}_1.fq.gz" "${inp}/${1}_2.fq.gz"
+      -l "${tmp}/${1}_1.fa" "${tmp}/${1}_2.fa"
   else
+
     # Single read annotation
-    mifaser -s 0 -S 0 -q -f "${inp}/${1}.fq.gz" -o "${tmp}" -d "GS-21-all"
+    seqtk seq -A "${inp}/${1}.fq.gz" > "${tmp}/${1}.fa"
+    mifaser -s 0 -S 0 -q -f "${tmp}/${1}.fa" -o "${tmp}" -d "GS-21-all"
   fi
 
   # Compress files and move them out of temp directory
+  rm "${tmp}"/*.fa
   mkdir -p "${tmp}/${1}"
-  ls -p "${tmp}/"* | grep -v / | xargs -I {} mv {} "${tmp}/${1}/."
+  ls -dp "${tmp}/"* | grep -v '/$' | xargs -I {} mv {} "${tmp}/${1}/."
   tar -C "${tmp}" -zcf "${tmp}/${1}.tar.gz" "${1}"
   chmod 775 "${tmp}/${1}.tar.gz"
   mv "${tmp}/${1}.tar.gz" "${out}/."
+
+  log "  Finished with ${1}"
 fi
